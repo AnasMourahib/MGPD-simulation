3feba06005fe020bd15384c1881920d5cf3776ea
AnasMourahib <anas.mourahib93@gmail.com>
2024-09-02 00:54:00 +0200

    Sample of size 100 from a transformed mgp random vector associated with the mixture Hüsler-Reiss model

--- Test_functions.R
+++ Test_functions.R
@@ -96,12 +96,73 @@ sample_mixture_HR<-function(d,r,Sigma,A,N){
   return(final)
 }
 set.seed(7)
-Y_mix_HR<-sample_mixture_HR(d,r,Sigma,A,1)
+Y_mix_HR<-sample_mixture_HR(d,r,Sigma,A,N)
 Z_mix_HR<-4*(exp(Y_mix_HR/4)-1)
 
 
+new<-rep(0,100)
+new[which(Z_mix_HR[1, ] > -4 | Z_mix_HR[2, ] > -4 | Z_mix_HR[3, ] > -4)]<-1
+new[which(Z_mix_HR[1, ] == -4 & Z_mix_HR[2, ] > -4 & Z_mix_HR[3,]>-4)]<-2
+new[which(Z_mix_HR[1, ] == -4 & Z_mix_HR[2, ] == -4 & Z_mix_HR[3,]>-4)]<-3
 
 
+Z_mix_HR<-as.data.frame(t(Z_mix_HR))
+Z_mix_HR$new<-new
+
+
+# Define the colors for the points
+my_cols <- c("#00AFBB", "#E7B800", "#FC4E07")
+
+# Set up the layout with an extra space for the legend
+layout(matrix(c(1, 2, 3, 4), nrow = 2, byrow = TRUE), widths = c(1, 1), heights = c(1, 1))
+
+# Adjust the margin and distance between the axis and labels
+par(mgp = c(4, 1, 0))  # Adjust distance: mgp[1] is axis title, mgp[2] is axis labels, mgp[3] is axis line
+
+# Plot 1: Z2 vs Z1
+plot(Z_mix_HR[, 2]~Z_mix_HR[, 1], 
+     pch = c(1,2,3)[Z_mix_HR$new], 
+     col = my_cols[Z_mix_HR$new], 
+     cex = 1.5, 
+     xlim = c(-4, 7), 
+     ylim = c(-4, 7), 
+     ylab = "", 
+     xlab = "")
+title(xlab = expression(Z[1]), ylab = expression(Z[2]), line = 2.3, cex.lab = 1.8)
+
+# Plot 2: Z3 vs Z1
+plot(Z_mix_HR[, 3]~Z_mix_HR[, 1], 
+     pch = c(1,2,3)[Z_mix_HR$new], 
+     col = my_cols[Z_mix_HR$new], 
+     cex = 1.5, 
+     xlim = c(-4, 7), 
+     ylim = c(-4, 7), 
+     ylab = "", 
+     xlab = "")
+title(xlab = expression(Z[1]), ylab = expression(Z[3]), line = 2.3, cex.lab = 1.8)
+
+# Plot 3: Z3 vs Z2
+plot(Z_mix_HR[, 3]~Z_mix_HR[, 2], 
+     pch = c(1,2,3)[Z_mix_HR$new], 
+     col = my_cols[Z_mix_HR$new], 
+     cex = 1.5, 
+     xlim = c(-4, 7), 
+     ylim = c(-4, 7), 
+     ylab = "", 
+     xlab = "")
+title(xlab = expression(Z[2]), ylab = expression(Z[3]), line = 2.3, cex.lab = 1.8)
+
+# Create a new plot space for the legend
+plot.new()
+
+# Draw the legend in the center of the plotting area with a box around it
+legend("center", 
+       pch = c(1,2,3), 
+       col = my_cols, 
+       legend = c(expression(A["{1,2,3}"]^{-4}), expression(A["{2,3}"]^{-4}), expression(A["{3}"]^{-4})), 
+       cex = 2, 
+       box.col = "black", 
+       box.lty = "solid")
 
 
 
